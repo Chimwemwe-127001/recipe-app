f0ce0405378a09c912449f779733432e70e62385
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2022-04-13 22:26:09 +0200

    Implement Recipe list

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+   user ||= User.new
+    if user.role == 'admin'
+      can :manage, :all
+    else
+      can :manage, Recipe, user_id: user.id
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :recipes, dependent: :destroy
+  validates :name, presence: true
 end
